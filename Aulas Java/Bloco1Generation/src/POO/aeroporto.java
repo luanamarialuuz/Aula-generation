855a0374538c359a542ce1aea54af968812e2d98
luanamariadaluzbarbosa@gmail.com <luanamariadaluzbarbosa@gmail.com>
2022-07-12 21:54:46 -0300

    Bloco  - Generation

--- Aulas Java/Bloco1Generation/src/POO/aeroporto.java
+++ Aulas Java/Bloco1Generation/src/POO/aeroporto.java
@@ -2,6 +2,13 @@ package POO;
 
 public class aeroporto {
 
+	public static void main(String[] args) {
+		
 	Aviao gol = new Aviao ("Luana", false);
+	gol.estado();
+	gol.PassageirosEmPe();
+	gol.PassageirosSentados();
+	
+	}
 	
 }
